ddc9463b431ed3e27e01d96a6045ad23d4eefdaa
li6612773steven <Waitingyou6612773>
2021-04-10 17:04:41 +0800

    projectHome

--- src/main/java/com/sjli/Arithmetic.java
+++ src/main/java/com/sjli/Arithmetic.java
@@ -1,4 +1,15 @@
 package com.sjli;
 
 public class Arithmetic {
+    public static void main(String[] args) {
+        // 四则运算
+        int i = (100 + 200) * (99 - 88); // 3300
+        int n = 7 * (5 + (i - 9)); // 23072
+        System.out.println(i);
+        System.out.println(n);
+        //除法
+        int x = 12345 / 67; // 184
+        //求余
+        int y = 12345 % 67;
+    }
 }
