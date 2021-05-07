db3d2b0b4c426aac986256d0bf85a3fe30e999c2
li6612773steven <li661273@163.com>
2021-05-07 11:33:16 +0800

    20210507office

---
+++ src/main/java/com/sjli/D01_EXC_TryCatch.java
@@ -0,0 +1,9 @@
+package com.sjli;
+
+public class D01_EXC_TryCatch {
+    public static void main(String[] args) {
+    //捕获异常
+        //在Java中，凡是可能抛出异常的语句，都可以用try ... catch捕获。把可能发生异常的语句放在try { ... }中，
+        // 然后使用catch捕获对应的Exception及其子类。
+    }
+}
